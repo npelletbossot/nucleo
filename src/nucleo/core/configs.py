a1d7d6c53cdf52adc1462264814bd121e20d60f2
Nicolas_PelletBossot <nicolas.pellet.tlse@gmail.com>
2026-09-25 19:54:26 +0200

    Y2026 M09 D25

--- src/nucleo/core/configs.py
+++ src/nucleo/core/configs.py
@@ -598,7 +598,7 @@ def choose_configuration(config: str) -> dict:
                 "land": np.array(['random', 'periodic', 'homogen']),
                 "s": np.array([150], dtype=int),
                 "l": np.array([10], dtype=int),
-                "bpmin": np.array([0], dtype=int)
+                "bpmin": np.array([0, 5, 10, 15, 20], dtype=int)
             },
             "probas": {
                 **ONESTEP__BASE["probas"],
